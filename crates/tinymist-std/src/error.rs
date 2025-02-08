49f778088b978cad15521cfba41fd37842d613ce
Myriad-Dreamin <camiyoru@gmail.com>
2025-02-08 13:29:42 +0800

    fix: bad typst gate

--- crates/tinymist-std/src/error.rs
+++ crates/tinymist-std/src/error.rs
@@ -2,12 +2,12 @@
 
 use core::fmt;
 
-use ecow::{EcoString, EcoVec};
+use ecow::EcoString;
 use serde::{Deserialize, Serialize};
 #[cfg(feature = "typst")]
 use typst::diag::SourceDiagnostic;
 
-use crate::debug_loc::LspRange;
+use lsp_types::Range as LspRange;
 
 /// The severity of a diagnostic message, following the LSP specification.
 #[derive(serde_repr::Serialize_repr, serde_repr::Deserialize_repr, Debug, Clone)]
@@ -63,7 +63,7 @@ pub enum ErrKind {
     Msg(EcoString),
     /// A source diagnostic message.
     #[cfg(feature = "typst")]
-    RawDiag(EcoVec<SourceDiagnostic>),
+    RawDiag(ecow::EcoVec<SourceDiagnostic>),
     /// A source diagnostic message.
     Diag(Box<DiagMessage>),
     /// An inner error.
@@ -197,6 +197,7 @@ impl fmt::Display for Error {
         if err.loc.is_empty() {
             match &err.kind {
                 ErrKind::Msg(msg) => write!(f, "{msg} with {:?}", err.args),
+                #[cfg(feature = "typst")]
                 ErrKind::RawDiag(diag) => {
                     write!(f, "{diag:?} with {:?}", err.args)
                 }
@@ -209,6 +210,7 @@ impl fmt::Display for Error {
         } else {
             match &err.kind {
                 ErrKind::Msg(msg) => write!(f, "{}: {} with {:?}", err.loc, msg, err.args),
+                #[cfg(feature = "typst")]
                 ErrKind::RawDiag(diag) => {
                     write!(f, "{}: {diag:?} with {:?}", err.loc, err.args)
                 }
@@ -229,8 +231,8 @@ impl From<anyhow::Error> for Error {
 }
 
 #[cfg(feature = "typst")]
-impl From<EcoVec<SourceDiagnostic>> for Error {
-    fn from(e: EcoVec<SourceDiagnostic>) -> Self {
+impl From<ecow::EcoVec<SourceDiagnostic>> for Error {
+    fn from(e: ecow::EcoVec<SourceDiagnostic>) -> Self {
         Error::new("", ErrKind::RawDiag(e), None)
     }
 }
