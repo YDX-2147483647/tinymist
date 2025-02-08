08f0c8c4701409f0cba3883237afe2f2a87fc11c
Myriad-Dreamin <camiyoru@gmail.com>
2025-02-08 14:07:11 +0800

    feat: add html document varant
    html changes
    
    html changes
    
    html changes

--- crates/tinymist/src/resource/symbols.rs
+++ crates/tinymist/src/resource/symbols.rs
@@ -1116,10 +1116,15 @@ fn trait_symbol_fonts(
 
     impl Worker<'_> {
         fn work(&mut self, doc: &TypstDocument) {
-            let TypstDocument::Paged(paged_doc) = doc;
-            for (pg, s) in paged_doc.pages.iter().zip(self.symbols.iter()) {
-                self.active = s;
-                self.work_frame(&pg.frame);
+            match doc {
+                TypstDocument::Paged(paged_doc) => {
+                    for (pg, s) in paged_doc.pages.iter().zip(self.symbols.iter()) {
+                        self.active = s;
+                        self.work_frame(&pg.frame);
+                    }
+                }
+                // todo: handle html
+                TypstDocument::Html(..) => {}
             }
         }
 
